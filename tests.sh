41b34461a4ee462eb57584053b76bd4f178bf378
Robert Lehmann <me@robertlehmann.de>
2009-11-12 14:50:38 +0100

    tested \n behaviour

--- tests.sh
+++ tests.sh
@@ -8,4 +8,5 @@ assert "cat" "bar" "bar"      # output expected if input's given
 assert "true" "" "" 0         # status code expected
 assert "exit 127" "" "" 127   # status code expected
 assert "echo > spam" "> spam" # no redirections
+assert "seq 2" "1\n2"         # multi-line output expected
 assert_end
