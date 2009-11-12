a69d7fbfb350bb030be3baa0b2f940389afea2aa
Robert Lehmann <me@robertlehmann.de>
2009-11-12 14:48:49 +0100

    added basic tests

---
+++ tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. assert.sh
+
+assert "echo"                 # no output expected
+assert "echo foo" "foo"       # output expected
+assert "cat" "bar" "bar"      # output expected if input's given
+assert "true" "" "" 0         # status code expected
+assert "exit 127" "" "" 127   # status code expected
+assert "echo > spam" "> spam" # no redirections
+assert_end
