d0d48b1fbb71d3405b2f2b9182293e13e67410ee
Robert Lehmann <me@robertlehmann.de>
2009-11-15 17:10:26 +0100

    commented output tests

--- tests.sh
+++ tests.sh
@@ -12,27 +12,34 @@ assert "seq 2" "1\n2"               # multi-line output expected
 assert_end demo
 
 _clean() {
-    _assert_reset
-    DEBUG= STOP= INVARIANT=1 DISCOVERONLY=
-    eval $*
+    _assert_reset # reset state
+    DEBUG= STOP= INVARIANT=1 DISCOVERONLY= # reset flags
+    eval $* # read new flags
 }
 
+# clean output
 assert "_clean; assert true; assert_end" \
 "all 1 tests passed."
+# error reports on failure
 assert "_clean; assert 'seq 1'; assert_end" \
 'test #1 "seq 1" failed:\n\texpected nothing\n\tgot "1"\n1 of 1 tests failed.'
 assert "_clean; assert true '1'; assert_end" \
 'test #1 "true" failed:\n\texpected "1"\n\tgot nothing\n1 of 1 tests failed.'
+# debug output (-v)
 assert "_clean DEBUG=1; assert true; assert_end" \
 ".\nall 1 tests passed."
 assert "_clean DEBUG=1; assert_raises false; assert_end" \
 'X\ntest #1 "false" failed:\n\tprogram terminated with code 1 instead of 0
 1 of 1 tests failed.'
+# collect tests only (-d)
 assert "_clean DISCOVERONLY=1; assert true; assert false; assert_end" \
 "collected 2 tests."
+# stop immediately on failure (-x)
 assert "_clean STOP=1 assert_raises false; assert_end" \
 'test #1 "false" failed:\n\tprogram terminated with code 1 instead of 0'
+# runtime statistics (omission of -i)
 assert_raises "_clean INVARIANT=;
 assert_end | egrep 'all 0 tests passed in [0-9].[0-9]{3}s'"
+# assert_end exit code is the number of failures
 assert_raises "_clean; assert_raises false; assert_raises false; assert_end" 2
 assert_end output
