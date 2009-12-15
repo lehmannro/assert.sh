caab358d8449f81c658ae9389314b4bcd20985b4
Robert Lehmann <me@robertlehmann.de>
2009-12-15 12:26:36 +0100

    added test for variables

--- tests.sh
+++ tests.sh
@@ -2,13 +2,14 @@
 
 . assert.sh
 
-assert "echo"                       # no output expected
-assert "echo foo" "foo"             # output expected
-assert "cat" "bar" "bar"            # output expected if input's given
-assert_raises "true" 0 ""           # status code expected
-assert_raises "exit 127" 127 ""     # status code expected
-assert "head -1 < $0" "#!/bin/bash" # redirections
-assert "seq 2" "1\n2"               # multi-line output expected
+assert "echo"                           # no output expected
+assert "echo foo" "foo"                 # output expected
+assert "cat" "bar" "bar"                # output expected if input's given
+assert_raises "true" 0 ""               # status code expected
+assert_raises "exit 127" 127 ""         # status code expected
+assert "head -1 < $0" "#!/bin/bash"     # redirections
+assert "seq 2" "1\n2"                   # multi-line output expected
+assert_raises 'read a; exit $a' 42 "42" # variables still work
 assert_end demo
 
 _clean() {
